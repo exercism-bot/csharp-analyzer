5a62fb8e949935d20d167f69f62d8864e1245c80
Erik Schierboom <erik_schierboom@hotmail.com>
2020-10-23 10:55:00 +0200

    Mimic Docker production settings

--- analyze-in-docker.ps1
+++ analyze-in-docker.ps1
@@ -29,4 +29,10 @@ param (
 )
 
 docker build -t exercism/csharp-analyzer .
-docker run -v ${InputDirectory}:/input -v ${OutputDirectory}:/output exercism/csharp-analyzer $Exercise /input /output
+docker run `
+  --network none `
+  --read-only `
+  --mount type=bind,src=${InputDirectory},dst=/input/ `
+  --mount type=bind,src=${OutputDirectory},dst=/output/ `
+  --mount type=tmpfs,dst=/tmp `
+  exercism/csharp-analyzer $Exercise /input /output
